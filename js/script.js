52597930439c424cc29f313e74a8fb37981037f9
Millard Seto <millardseto@mac.com>
2017-05-02 18:38:06 -0700

    Adds image type

--- js/script.js
+++ js/script.js
@@ -149,56 +149,64 @@ $(function() {
       "price": 26.99,
       "description": "Two classic patterns in one great look: This supersoft and cozy reversible scarf instantly doubles your street-style cred. 100% acrylic.",
       "imageTitle": "reversible-plaid.jpg",
-      "imageAlt":"reversible plaid scarf"
+      "imageAlt":"reversible plaid scarf",
+      "type":"s"
     },
     {
       "name": "Wool Cable Knit",
       "price": 49.99,
       "description": "Warm yourself with this women's natural cable knit scarf, crafted from 100% Merino wool. Imported.",
       "imageTitle": "wool-cable.jpeg",
-      "imageAlt":""
+      "imageAlt":"wool cable knit scarf",
+      "type":"s"
     },
     {
       "name": "Northern Lights",
       "price": 29.99,
       "description": "Handmade by women in Agra, sales provide medical and educational support in this remote area of India. Crinkly 100% cotton.",
       "imageTitle": "northern-lights.jpg",
-      "imageAlt":""
+      "imageAlt":"norther lights scarf",
+      "type":"s"
     },
     {
       "name": "Ombre Infinity",
       "price": 11.99,
       "description": "A dip-dye effect adds color and dimension to a cozy infinity scarf featuring a soft, chunky knit. 100% acrylic.",
       "imageTitle": "ombre-infinity.jpg",
-      "imageAlt":"ombre infinity scarf"
+      "imageAlt":"ombre infinity scarf",
+      "type":"s"
     },
     {
       "name": "Fringed Plaid",
       "price": 18.99,
       "description": "Generously sized, extra soft and featuring a dazzling fringe, this scarf is rendered in a versatile gray, black and white plaid. Expertly beat the cold with style. 100% acrylic.",
       "imageTitle": "fringed-plaid.jpeg",
-      "imageAlt":"fringed plaid scarf"
+      "imageAlt":"fringed plaid scarf",
+      "type":"s"
     },
     {
       "name": "Multi Color",
       "price": 22.99,
       "description": "The Who What Wear Oversize Color-Block Square Scarf is big, bold, and designed to twist and wrap any way you wish. All the colors of the season are harmonized in this oversize accent, so you can adjust to contrast or match your outfit; soft and lush, it’s your stylish standoff against cold AC and unexpected fall breezes. 100% acrylic",
       "imageTitle": "multi-color.jpeg",
-      "imageAlt":"multi color scarf"
+      "imageAlt":"multi color scarf",
+      "type":"s"
     },
     {
       "name": "Etro Paisley-Print Silk",
       "price": 249.99,
       "description": "Luxurious silk scarf with subtle paisley pattern. 100% silk",
       "imageTitle": "etro.png",
-      "imageAlt":"northern lights scarf"
+      "imageAlt":"northern lights scarf",
+      "type":"s"
     },
     {
       "name": "Ashby Twill",
       "price": 70.99,
       "description": "Faribault brings you the Ashby Twill Scarf in Natural. Woven with a 'broken' twill technique, the Ashby Twill Scarf has a slight zigzag texture. Made in USA, this timeless scarf is crafted with luxurious merino wool and finished with heather gray fringe. 100% Merino wool",
       "imageTitle": "twill.jpg",
-      "imageAlt":"ashby twill scarf"
+      "imageAlt":"ashby twill scarf",
+      "type":"s"
     }
   ]
 
