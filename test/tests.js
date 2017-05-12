5fb546e89743dbcf1acc8eeee82d6aad919a28bd
Millard Seto <millardseto@mac.com>
2017-05-11 18:07:56 -0700

    more test framework setup

--- test/tests.js
+++ test/tests.js
@@ -1,3 +1,6 @@
-QUnit.test( "hello test", function( assert ) {
-  assert.ok( 1 == "1", "Passed!" );
+$q(function() {   // fire event when iframe is ready
+  $q('#testframe').on('load', function() {
+        // Get the jQuery Object from the original code
+        $ = window.frames[0].jQuery;
+    });
 });
